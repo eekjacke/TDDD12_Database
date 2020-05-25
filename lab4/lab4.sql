9dd728b6e337d583d4e6820228edc79e407bca52
Jacob Eek <jacee719@student.liu.se>
2020-05-25 21:26:00 +0200

    6an kanske klar

--- lab4/lab4.sql
+++ lab4/lab4.sql
@@ -19,7 +19,11 @@ DROP PROCEDURE IF EXISTS addDestination;
 DROP PROCEDURE IF EXISTS addRoute;
 DROP PROCEDURE IF EXISTS addFlight;
 DROP FUNCTION IF EXISTS calculateFreeSeats;
-
+DROP FUNCTION IF EXISTS calculatePrice;
+DROP PROCEDURE IF EXISTS addReservation;
+DROP PROCEDURE IF EXISTS addPassenger;
+DROP PROCEDURE IF EXISTS addContact;
+DROP PROCEDURE IF EXISTS addPayment;
 
 SELECT 'Creating tables' AS 'Message';
 
@@ -90,8 +94,8 @@ CREATE TABLE contact
      
 CREATE TABLE card
 	(card_num BIGINT,
-     holder_first_name VARCHAR(99),
-     holder_last_name VARCHAR(99),
+     holder_first_name VARCHAR(30),
+     holder_last_name VARCHAR(30),
      CONSTRAINT pk_card PRIMARY KEY(card_num)) ENGINE=InnoDB;
      
 CREATE TABLE reserved_passenger
@@ -164,64 +168,166 @@ BEGIN
 	UNTIL week = 53
     END REPEAT;
 END;
-
-/* TEST SCRIPT TASK 3 */
-/*
-SELECT "Testing questions for flight addition procedures, i.e. question 3" as "Message";
-SELECT "Expected output for all queries are 'Query OK, 1 row affected (0.00 sec)' (where the time might differ)" as "Message";
-*/
-/*Fill the database with data */
-
-/*Fill the database with data */
-SELECT "Trying to add 2 years" AS "Message";
-CALL addYear(2010, 2.3);
-CALL addYear(2011, 2.5);
-SELECT "Trying to add 4 days" AS "Message";
-CALL addDay(2010,"Monday",1);
-CALL addDay(2010,"Tuesday",1.5);
-CALL addDay(2011,"Saturday",2);
-CALL addDay(2011,"Sunday",2.5);
-SELECT "Trying to add 2 destinations" AS "Message";
-CALL addDestination("MIT","Minas Tirith Airport","Mordor","Minas Tirith");
-CALL addDestination("HOB","Hobbiton Airport","The Shire", "Hobbiton");
-
-SELECT "Trying to add 4 routes" AS "Message";
-CALL addRoute("MIT","HOB",2010,2000);
-CALL addRoute("HOB","MIT",2010,1600);
-CALL addRoute("MIT","HOB",2011,2100);
-CALL addRoute("HOB","MIT",2011,1500);
-
-SELECT "Trying to add 4 weeklyschedule flights" AS "Message";
-CALL addFlight("MIT","HOB", 2010, 1, "09:00:00");
-CALL addFlight("HOB","MIT", 2010, 2, "10:00:00");
-CALL addFlight("MIT","HOB", 2011, 3, "11:00:00");
-CALL addFlight("HOB","MIT", 2011, 4, "12:00:00");
-
-
-/*
-SELECT "You are now supposed to have 208 flights in your database. If so, and with reasonable data, it is probably correct and this is further tested for question 7" as "Message";
-*/
+//
 
 CREATE FUNCTION calculateFreeSeats(flightnumber INT) RETURNS INT
 BEGIN
 	DECLARE free_seats INT;
-    DECLARE reserved_seats INT;
-	SELECT sum(num_of_seats) FROM reservation WHERE flight_num=flightnumber INTO reserved_seats;
-    SET free_seats = 40 - reserved_seats;
+    DECLARE booked_seats INT;
+
+    SELECT sum(num_of_seats) FROM reservation WHERE reservation_num IN
+		(SELECT booking_id FROM booking) AND flight_num=flightnumber INTO booked_seats;
+    
+    SET free_seats = 40 - booked_seats;
     RETURN free_seats;
     /* 40 passengers */
     /* kanske måste kolla på något med att ticker_num = NULL för att ej blanda ihop reservationer och bokningar */
 END;
+//
 
-/* TEST CODE FOR calculateFreeSeats */
-INSERT INTO reservation (flight_num, num_of_seats) values (1,1);
-INSERT INTO reservation (flight_num, num_of_seats) values (2,40);
-INSERT INTO reservation (flight_num, num_of_seats) values (3,43);
-SELECT* from reservation;
-SELECT calculateFreeSeats(3);
-/* TEST CODE FOR calculateFreeSeats */
-
-CREATE FUNCTION calculatePrice(flightnumber INT) RETURNS DOUBLE;
+CREATE FUNCTION calculatePrice(flightnumber INT) RETURNS DOUBLE
 BEGIN
-/* tot_price = routeprice * weekdayfactor * (bookedpassengers+1)/40 * profitfactor */
+	DECLARE routeprice DOUBLE;
+    DECLARE weekdayfactor DOUBLE;
+    DECLARE freeseats INT;
+    DECLARE bookedseats INT;
+    DECLARE profitfactor DOUBLE;
+    DECLARE total_price DOUBLE;
+    
+	SELECT route_price FROM route WHERE route_id IN
+		(SELECT route FROM weekly_schedule WHERE schedule_id IN 
+			(SELECT weekly_flight FROM flight WHERE flight_num = flightnumber)) INTO routeprice;
+            
+	SELECT weekday_factor FROM weekday WHERE id IN
+		(SELECT weekday_id FROM weekly_schedule WHERE schedule_id IN
+			(SELECT weekly_flight FROM flight WHERE flight_num = flightnumber)) INTO weekdayfactor;
+            
+    SELECT year_factor FROM year WHERE year IN    
+		(SELECT year FROM weekday WHERE id IN
+			(SELECT weekday_id FROM weekly_schedule WHERE schedule_id IN
+				(SELECT weekly_flight FROM flight WHERE flight_num = flightnumber))) INTO profitfactor;
+                
+	SELECT calculateFreeSeats(flightnumber) INTO freeseats;
+    SET bookedseats = 40 - freeseats;
+    
+    SET total_price = routeprice*weekdayfactor*((bookedseats+1)/40)*profitfactor;
+    RETURN total_price;
 END;
+//
+
+CREATE PROCEDURE addReservation(IN departure_airport_code VARCHAR(3), IN arrival_airport_code VARCHAR(3), IN year INT, IN week INT, IN day VARCHAR(10), IN time TIME, IN number_of_passengers INT, OUT output_reservation_nr INT)
+BEGIN
+	DECLARE flight_number INT;
+    DECLARE free_seats INT;
+    SELECT flight_num FROM flight WHERE weekly_flight IN 
+		(SELECT schedule_id FROM weekly_schedule WHERE route IN
+			(SELECT route_id FROM route WHERE airport_code_dept = departure_airport_code AND airport_code_arr = arrival_airport_code))
+            
+		AND weekly_flight IN (SELECT schedule_id FROM weekly_schedule WHERE weekday_id IN
+			(SELECT id FROM weekday WHERE name = day AND weekday.year=year))
+            
+		AND weekly_flight IN (SELECT schedule_id FROM weekly_schedule where time_of_dept = time)
+        
+        AND flight.week_num = week
+        
+        INTO flight_number;
+        
+    IF flight_number = NULL
+    THEN SELECT 'Flight doesnt exist' AS 'Message';
+    ELSEIF calculateFreeSeats(flight_number) < number_of_passengers 
+	THEN SELECT 'No free seats left' AS 'Message';
+    ELSE INSERT INTO reservation (num_of_seats, flight_num) values (number_of_passengers, flight_number);
+    END IF;
+    
+	SELECT last_insert_id() INTO output_reservation_nr;
+END;
+//
+
+CREATE PROCEDURE addPassenger(IN reservation_number INT, IN passport_number INT, IN first_name VARCHAR(30), IN last_name VARCHAR(30))
+BEGIN
+	IF NOT EXISTS (SELECT * FROM passenger WHERE passenger.passport_num = passport_number)
+    THEN INSERT INTO passenger(passport_num, first_name, last_name) VALUES (passport_number, first_name, last_name);
+    END IF;
+    INSERT INTO reserved_passenger (reservation_num, passport_num) VALUES (reservation_number, passport_number);
+END;
+//
+
+CREATE PROCEDURE addContact(IN reservation_number INT, IN passport_number INT, IN email VARCHAR(30), IN phone BIGINT)
+BEGIN
+	IF NOT EXISTS (SELECT* FROM reserved_passenger WHERE reservation_num=reservation_number AND passport_num = passport_number)
+    THEN SELECT 'The person is not added as a passenger to the reservation' AS 'Message';
+    ELSE INSERT INTO contact (passport_num, phone_num, email) VALUES (passport_number, phone, email);
+    END IF;
+END;
+//
+
+CREATE PROCEDURE addPayment(IN reservation_number INT , IN cardholder_firstname VARCHAR(30), IN cardholder_lastname VARCHAR(30), IN credit_card_number BIGINT)
+BEGIN
+	DECLARE flight_number INT;
+    SELECT flight_num FROM reservation WHERE reservation_num = reservation_number INTO flight_number;
+    
+	IF (SELECT contact FROM reservation WHERE reservation_num = reservation_number) = NULL
+    THEN SELECT 'There is no contact for this reservation' AS 'Message';
+    ELSEIF calculateFreeSeats(flight_number) < number_of_passengers 
+	THEN SELECT 'No free seats left' AS 'Message';
+    ELSE INSERT INTO card (card_num, holder_first_name, holder_last_name) VALUES (credit_card_number, cardholder_firstname, cardholder_lastname);
+		 INSERT INTO booking (booking_id, price, card_num) VALUES (reservation_number, calculatePrice(flight_number), credit_card_number);
+		 INSERT INTO reserved_passenger(ticket_num) VALUES (rand());
+    END IF;
+END;
+//
+
+/******************************************************************************************
+ Question 7, Correct representation in the view.
+ This is a test script that tests that the interface of the BryanAir back-end works
+ correctly. More specifically it tests that flights and reservations are added correctly and
+ that the number of seats and price is calculated correctly. This is done by checking against a 
+ previous (correct) response to the query from an external database. 
+**********************************************************************************************/
+
+SELECT "Checking that bookings and flights are added correctly by checking the view" as "Message";
+/*Fill the database with flights */
+SELECT "Step1, fill the database with flights" AS "Message";
+CALL addYear(2010, 2.3);
+CALL addYear(2011, 2.5);
+CALL addDay(2010,"Monday",1);
+CALL addDay(2010,"Tuesday",1.5);
+CALL addDay(2011,"Saturday",2);
+CALL addDay(2011,"Sunday",2.5);
+CALL addDestination("MIT","Minas Tirith","Mordor");
+CALL addDestination("HOB","Hobbiton","The Shire");
+CALL addRoute("MIT","HOB",2010,2000);
+CALL addRoute("HOB","MIT",2010,1600);
+CALL addRoute("MIT","HOB",2011,2100);
+CALL addRoute("HOB","MIT",2011,1500);
+CALL addFlight("MIT","HOB", 2010, "Monday", "09:00:00");
+CALL addFlight("HOB","MIT", 2010, "Tuesday", "10:00:00");
+CALL addFlight("MIT","HOB", 2011, "Sunday", "11:00:00");
+CALL addFlight("HOB","MIT", 2011, "Sunday", "12:00:00");
+
+SELECT "Step2, add a bunch of bookings to the flights" AS "Message";
+CALL addReservation("MIT","HOB",2010,1,"Monday","09:00:00",3,@a); 
+CALL addPassenger(@a,00000001,"Frodo Baggins"); 
+CALL addContact(@a,00000001,"frodo@magic.mail",080667989); 
+CALL addPassenger(@a,00000002,"Sam Gamgee"); 
+CALL addPassenger(@a,00000003,"Merry Pippins");
+CALL addPayment (@a, "Gandalf", 6767); 
+CALL addReservation("MIT","HOB",2010,1,"Monday","09:00:00",3,@b); 
+CALL addPassenger(@b,00000011,"Nazgul1"); 
+CALL addContact(@b,00000011,"Nazgul@darkness.mail",666); 
+CALL addPassenger(@b,00000012,"Nazgul2"); 
+CALL addPassenger(@b,00000013,"Nazgul3");
+CALL addPayment (@b, "Saruman", 6868); 
+
+SELECT "Step3, check that the results are correct. If so the next query should return the empty set. If any line is returned then this is either missing, incorrect or additional to what the database should contain" AS "Message";
+SELECT departure_city_name, destination_city_name, departure_time, departure_day,departure_week, departure_year, nr_of_free_seats, current_price_per_seat
+FROM (
+SELECT departure_city_name, destination_city_name, departure_time, departure_day,departure_week, departure_year, nr_of_free_seats, current_price_per_seat FROM allFlights
+UNION ALL
+SELECT departure_city_name, destination_city_name, departure_time, departure_day,departure_week, departure_year, nr_of_free_seats, current_price_per_seat FROM TDDD37.Question7CorrectResult
+) res
+GROUP BY departure_city_name, destination_city_name, departure_time, departure_day,departure_week, departure_year, nr_of_free_seats, current_price_per_seat
+HAVING count(*) = 1;
+
+
+
